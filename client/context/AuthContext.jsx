d11acfe5083f771d328622b5c2afb113acedae3f
Babu-Vadaboina <tatababuvadaboina@gmail.com>
2025-09-17 16:10:57 +0530

    client - adding login auth functionality to home page - 4.20

--- client/context/AuthContext.jsx
+++ client/context/AuthContext.jsx
@@ -91,6 +91,9 @@ export const AuthProvider = ({ children }) => {
     authUser,
     onlineUsers,
     socket,
+    login,
+    logout,
+    updateProfile,
   };
 
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
